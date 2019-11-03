5ea28304c661c34302847216edd21bdc2d8d126b
Ryan Alameddine <rhalameddine@gmail.com>
2019-11-03 12:29:39 -0800

    Added NandGate

--- WireForm/Circuitry/Gates/NandGate.cs
+++ WireForm/Circuitry/Gates/NandGate.cs
@@ -7,9 +7,9 @@ using WireForm.MathUtils.Collision;
 
 namespace WireForm.Circuitry.Gates
 {
-    class AndGate : Gate
+    class NandGate : Gate
     {
-        public AndGate(Vec2 Position)
+        public NandGate(Vec2 Position)
             : base(Position, new BoxCollider(-2, -1.5f, 3, 3))
         {
             Inputs = new GatePin[] {
@@ -24,7 +24,7 @@ namespace WireForm.Circuitry.Gates
 
         protected override void compute()
         {
-            Outputs[0].Value = Inputs[0].Value.And(Inputs[1].Value);
+            Outputs[0].Value = Inputs[0].Value.And(Inputs[1].Value).Not();
         }
 
         protected override void draw(Graphics gfx)
@@ -34,6 +34,8 @@ namespace WireForm.Circuitry.Gates
             gfx._DrawLine(Color.Black, 10, StartPoint + new Vec2(-2, 1.5f), StartPoint + new Vec2(-.5f + .1f, 1.5f));
             gfx._DrawLine(Color.Black, 10, StartPoint + new Vec2(-2, -1.5f), StartPoint + new Vec2(-.5f + .1f, -1.5f));
             gfx._DrawArc(Color.Black, 10, StartPoint.X - 2f, StartPoint.Y - 1.5f, 3f, 3f, 270, 180);
+
+            gfx._DrawEllipseC(Color.Black, 10, StartPoint.X + 1, StartPoint.Y, .4f, .4f);
         }
 
         public override CircuitObject Copy()
